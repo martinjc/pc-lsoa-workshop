767eff1e09195a593d85938b6fb16eeca6759e38
Martin Chorley <martin.chorley@gmail.com>
2021-11-17 10:33:56 +0000

    thing

--- pc-lsoa.js
+++ pc-lsoa.js
@@ -1,11 +1,28 @@
-let inputBox = document.getElementById('pc_input')
-inputBox.addEventListener('changed', () => {
-    document.getElementsByClassName('hidden')[0].classList.toggle('hidden');
-})
+
+lookup = {}
 
 fetch('https://github.com/martinjc/pc-lsoa-workshop/blob/main/pc_geo_lookup.csv?raw=true')
 .then((data) => {
-    console.log(data);
+    let lines = data.split('\n');
+    lines.forEach((l, i) => {
+        if(i > 0) {
+            let columns = l.split(',');
+            lookup[columns[0]] = {'code': columns[1], 'name': columns[2]};
+        }
+    });
+    return lookup
+}).then(lookup => {
+    let output = document.getElementById('output').innerHTML(lookup);
 });
+ 
+
+
+let inputBox = document.getElementById('pc_input')
+inputBox.addEventListener('change', () => {
+    document.getElementsByClassName('hidden')[0].classList.toggle('hidden');
+
+})
+
+
 
 
