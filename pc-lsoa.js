85d3b43e77e1729f3e1278bca20eae262863ac11
Martin Chorley <martin.chorley@gmail.com>
2021-11-17 10:20:35 +0000

    maybe?

--- pc-lsoa.js
+++ pc-lsoa.js
@@ -1,1 +1,11 @@
+let inputBox = document.getElementById('pc_input')
+inputBox.addEventListener('changed', () => {
+    document.getElementsByClassName('hidden')[0].classList.toggle('hidden');
+})
+
+fetch('https://github.com/martinjc/pc-lsoa-workshop/blob/main/pc_geo_lookup.csv?raw=true')
+.then((data) => {
+    console.log(data);
+});
+
 
